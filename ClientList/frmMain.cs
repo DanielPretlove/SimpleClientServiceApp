b053ef970bac040e18061c21265148ad655e0d90
Daniel Pretlove <danielpretlove5@gmail.com>
2021-08-29 01:43:50 +1000

    first commit

---
+++ ClientList/frmMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientList
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+
+        }
+
+        private void dgvClientData_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dgvClientData.SelectedCells[0].Style
+   = new DataGridViewCellStyle { ForeColor = Color.Yellow };
+        }
+    }
+}
